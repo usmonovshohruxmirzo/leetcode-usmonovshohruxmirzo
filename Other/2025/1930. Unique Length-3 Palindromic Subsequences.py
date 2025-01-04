266b3238b28b84f92e0ffa5cc22bd13361890edc
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-01-04 23:34:19 +0500

    feat(Python): 1930. Unique Length-3 Palindromic Subsequences

---
+++ Other/2025/1930. Unique Length-3 Palindromic Subsequences.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        letters = set(s)
+        count = 0
+        
+        for letter in letters:
+            i, j = s.index(letter), s.rindex(letter)
+            between = set()
+
+            for k in range(i + 1, j):
+                between.add(s[k])
+                print(between)
+
+            count += len(between)
+
+        return count
+# Test Cases
+solution = Solution()
+# print(solution.countPalindromicSubsequence("aabca"))
+# print(solution.countPalindromicSubsequence("abc"))
+# print(solution.countPalindromicSubsequence("bbcbaba"))
