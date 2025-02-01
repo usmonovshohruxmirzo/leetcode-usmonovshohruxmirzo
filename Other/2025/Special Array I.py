36956b470bcf1a99b1a41e035273cdcf8dbe80c6
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-02-01 11:03:34 +0500

    feat(Python): Special Array I

---
+++ Other/2025/Special Array I.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        for i in range(len(nums) - 1):
+            if nums[i] % 2 == nums[i + 1] % 2:
+                return False
+        return True
