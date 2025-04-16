50117e7c253ad92680781e111cbda55ed2b808bf
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-04-16 17:17:28 +0500

    feat: 704. Binary Search

---
+++ Other/2025/704. Binary Search.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
+
+
+// nums = [-1,0,3,5,9,12]
+// targer = 9
