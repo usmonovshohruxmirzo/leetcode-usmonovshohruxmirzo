61d23e8be47365e7407994cf721da1529285e4bb
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-05-27 20:54:52 +0500

    feat(CS): 2894. Divisible and Non-divisible Sums Differenc

---
+++ Other/2025/2894. Divisible and Non-divisible Sums Difference.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int DifferenceOfSums(int n, int m) {
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % m == 0) {
+                ans -= i;
+            } else {
+                ans += i;
+            }
+        }
+        return ans;
+    }
+}
